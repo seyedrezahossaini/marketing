c909e1abe43ead1a9fda4ef4884af32c9ccd6aff
srh <s.hosseyni2013@gmail.com>
2017-09-24 17:05:37 +0330

    inprogress

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppContainer = () => (
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>
+);
+ReactDOM.render(<AppContainer />, document.getElementById('root'));
 registerServiceWorker();
